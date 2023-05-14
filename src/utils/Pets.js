28f55871273b460aec1cb3da32f10846848dae27
CryptoMagg <crypto.magg.42@gmail.com>
2023-05-14 13:46:13 +0200

    Untaint all the things

--- src/utils/Pets.js
+++ src/utils/Pets.js
@@ -1,20 +1,19 @@
-import { GraphQLClient, gql } from 'graphql-request';
+import {gql, GraphQLClient} from 'graphql-request';
 import axios from "axios";
 
 const greyEggData = require("@/data/greyEggData.json");
 const blueEggData = require("@/data/blueEggData.json");
 
 
-
 const graphqlapiUrl = 'https://defi-kingdoms-community-api-gateway-co06z8vi.uc.gateway.dev/graphql';
 
-const petQuery =  
-      gql`
-        query getPets($ownerAddress: String!){
-          pets(
-          where: {
-            owner: $ownerAddress
-          }) {
+const petQuery =
+gql`
+    query getPets($ownerAddress: String!){
+        pets(
+            where: {
+                owner: $ownerAddress
+            }) {
             id
             appearance
             rarity
@@ -24,55 +23,55 @@ const petQuery =
             combatBonus
             eggType
             originRealm
-          }
-        }`;
+        }
+    }`;
 
 const chainMap = {
-                "SER1": "sd",
-                "CRY":  "cv",
-                "SER2": "sd2"
-              }
-              export const Rarity = {
-                0: "Common",
-                1: "Uncommon",
-                2: "Rare",
-                3: "Legendary",
-                4: "Mythic"
-              };
+  "SER1": "sd",
+  "CRY": "cv",
+  "SER2": "sd2"
+}
+export const Rarity = {
+  0: "Common",
+  1: "Uncommon",
+  2: "Rare",
+  3: "Legendary",
+  4: "Mythic"
+};
 
 const petElements = {
-          0: "Fire",
-          1: "Water",
-          2: "Earth",
-          3: "Wind",
-          4: "Lightning",
-          5: "Ice",
-          6: "Light",
-          7: "Dark"
-        };
+  0: "Fire",
+  1: "Water",
+  2: "Earth",
+  3: "Wind",
+  4: "Lightning",
+  5: "Ice",
+  6: "Light",
+  7: "Dark"
+};
 
 const eggType = {
-          0: "Blue Egg",
-          1: "Grey Egg",
-          2: "Green Egg",
-          3: "Yellow Egg",
-          4: "Golden Egg"
-        };
+  0: "Blue Egg",
+  1: "Grey Egg",
+  2: "Green Egg",
+  3: "Yellow Egg",
+  4: "Golden Egg"
+};
 
 const bonuses = {
-  0:   "-",
-  1:   "★",
-  80:  "★★",
+  0: "-",
+  1: "★",
+  80: "★★",
   160: "★★★"
 };
 
 const bonusStars = {
-   0:   "★",
-   1:  "★★",
-   2: "★★★"
+  0: "★",
+  1: "★★",
+  2: "★★★"
 };
 
-const craftingTypes = {  
+const craftingTypes = {
   0: "Blacksmithing",
   1: "Goldsmithing",
   2: "Armorsmithing",
@@ -83,7 +82,7 @@ const craftingTypes = {
   7: "Alchemy"
 }
 
-const professions =  {
+const professions = {
   0: "Fishing",
   1: "Foraging",
   2: "Gardening",
@@ -91,168 +90,173 @@ const professions =  {
 
 const gatheringBonuses = {
   // Blue Pet Bonuses
-  0: { 
-     1: "Unrevealed",
-     80: "Unrevealed",
-     160: "Unrevealed",
-     2: "Efficient Angler",
-     81: "Efficient Angler",
-     161: "Efficient Angler",
-     3: "Bountiful Catch",
-     82: "Bountiful Catch",
-     162: "Bountiful Catch",
-     4: "Keen Eye",
-     83: "Keen Eye",
-     163: "Keen Eye",
-     5: "Fortune Seeker",
-     84: "Fortune Seeker",
-     164: "Fortune Seeker",
-     6: "Clutch Collector",
-     85: "Clutch Collector",
-     165: "Clutch Collector",
-     7: "Runic Discoveries",
-     86: "Runic Discoveries",
-     166: "Runic Discoveries",
-     8: "Skilled Angler",
-     87: "Skilled Angler",
-     167: "Skilled Angler",
-     9: "Astute Angler",
-     88: "Astute Angler",
-     168: "Astute Angler",
-     10: "Bonus Bounty",
-     89: "Bonus Bounty",
-     169: "Bonus Bounty",
-     11: "Gaia's Chosen",
-     90: "Gaia's Chosen",
-     170: "Gaia's Chosen",
-     171: "Innate Angler"
+  0: {
+    1: "Unrevealed",
+    80: "Unrevealed",
+    160: "Unrevealed",
+    2: "Efficient Angler",
+    81: "Efficient Angler",
+    161: "Efficient Angler",
+    3: "Bountiful Catch",
+    82: "Bountiful Catch",
+    162: "Bountiful Catch",
+    4: "Keen Eye",
+    83: "Keen Eye",
+    163: "Keen Eye",
+    5: "Fortune Seeker",
+    84: "Fortune Seeker",
+    164: "Fortune Seeker",
+    6: "Clutch Collector",
+    85: "Clutch Collector",
+    165: "Clutch Collector",
+    7: "Runic Discoveries",
+    86: "Runic Discoveries",
+    166: "Runic Discoveries",
+    8: "Skilled Angler",
+    87: "Skilled Angler",
+    167: "Skilled Angler",
+    9: "Astute Angler",
+    88: "Astute Angler",
+    168: "Astute Angler",
+    10: "Bonus Bounty",
+    89: "Bonus Bounty",
+    169: "Bonus Bounty",
+    11: "Gaia's Chosen",
+    90: "Gaia's Chosen",
+    170: "Gaia's Chosen",
+    171: "Innate Angler"
   },
 
   // Grey Pet Bonuses
   1: {
-     1: "Unrevealed",
-     80: "Unrevealed",
-     160: "Unrevealed",
-     2: "Efficient Scavenger",
-     81: "Efficient Scavenger",
-     161: "Efficient Scavenger",
-     3: "Bountiful Haul",
-     82: "Bountiful Haul",
-     162: "Bountiful Haul",
-     4: "Keen Eye",
-     83: "Keen Eye",
-     163: "Keen Eye",
-     5: "Fortune Seeker",
-     84: "Fortune Seeker",
-     164: "Fortune Seeker",
-     6: "Clutch Collector",
-     85: "Clutch Collector",
-     165: "Clutch Collector",
-     7: "Runic Discoveries",
-     86: "Runic Discoveries",
-     166: "Runic Discoveries",
-     8: "Skilled Scavenger",
-     87: "Skilled Scavenger",
-     167: "Skilled Scavenger",
-     9: "Astute Scavenger",
-     88: "Astute Scavenger",
-     168: "Astute Scavenger",
-     10: "Bonus Bounty",
-     89: "Bonus Bounty",
-     169: "Bonus Bounty",
-     11: "Gaia's Chosen",
-     90: "Gaia's Chosen",
-     170: "Gaia's Chosen",
-     171: "Innate Scavenger"
+    1: "Unrevealed",
+    80: "Unrevealed",
+    160: "Unrevealed",
+    2: "Efficient Scavenger",
+    81: "Efficient Scavenger",
+    161: "Efficient Scavenger",
+    3: "Bountiful Haul",
+    82: "Bountiful Haul",
+    162: "Bountiful Haul",
+    4: "Keen Eye",
+    83: "Keen Eye",
+    163: "Keen Eye",
+    5: "Fortune Seeker",
+    84: "Fortune Seeker",
+    164: "Fortune Seeker",
+    6: "Clutch Collector",
+    85: "Clutch Collector",
+    165: "Clutch Collector",
+    7: "Runic Discoveries",
+    86: "Runic Discoveries",
+    166: "Runic Discoveries",
+    8: "Skilled Scavenger",
+    87: "Skilled Scavenger",
+    167: "Skilled Scavenger",
+    9: "Astute Scavenger",
+    88: "Astute Scavenger",
+    168: "Astute Scavenger",
+    10: "Bonus Bounty",
+    89: "Bonus Bounty",
+    169: "Bonus Bounty",
+    11: "Gaia's Chosen",
+    90: "Gaia's Chosen",
+    170: "Gaia's Chosen",
+    171: "Innate Scavenger"
   },
 
   // Green Pet Bonuses
   2: {
-     1: "Unrevealed",
-     80: "Unrevealed",
-     160: "Unrevealed",
-     2: "Efficient Greenskeeper",
-     81: "Efficient Greenskeeper",
-     161: "Efficient Greenskeeper",
-     3: "Bountiful Harvest",
-     82: "Bountiful Harvest",
-     162: "Bountiful Harvest",
-     4: "Second Chance",
-     83: "Second Chance",
-     163: "Second Chance",
-     5: "Clutch Collector",
-     84: "Clutch Collector",
-     164: "Clutch Collector",
-     6: "Runic Discoveries",
-     85: "Runic Discoveries",
-     165: "Runic Discoveries",
-     7: "Skilled Greenskeeper",
-     86: "Skilled Greenskeeper",
-     166: "Skilled Greenskeeper",
-     8: "Astute Greenskeeper",
-     87: "Astute Greenskeeper",
-     167: "Astute Greenskeeper",
-     9: "Bonus Bounty",
-     88: "Bonus Bounty",
-     168: "Bonus Bounty",
-     10: "Gaia's Chosen",
-     89: "Gaia's Chosen",
-     169: "Gaia's Chosen",
-     90: "Power Surge",
-     170: "Power Surge",
-     171: "Innate Greenskeeper"
+    1: "Unrevealed",
+    80: "Unrevealed",
+    160: "Unrevealed",
+    2: "Efficient Greenskeeper",
+    81: "Efficient Greenskeeper",
+    161: "Efficient Greenskeeper",
+    3: "Bountiful Harvest",
+    82: "Bountiful Harvest",
+    162: "Bountiful Harvest",
+    4: "Second Chance",
+    83: "Second Chance",
+    163: "Second Chance",
+    5: "Clutch Collector",
+    84: "Clutch Collector",
+    164: "Clutch Collector",
+    6: "Runic Discoveries",
+    85: "Runic Discoveries",
+    165: "Runic Discoveries",
+    7: "Skilled Greenskeeper",
+    86: "Skilled Greenskeeper",
+    166: "Skilled Greenskeeper",
+    8: "Astute Greenskeeper",
+    87: "Astute Greenskeeper",
+    167: "Astute Greenskeeper",
+    9: "Bonus Bounty",
+    88: "Bonus Bounty",
+    168: "Bonus Bounty",
+    10: "Gaia's Chosen",
+    89: "Gaia's Chosen",
+    169: "Gaia's Chosen",
+    90: "Power Surge",
+    170: "Power Surge",
+    171: "Innate Greenskeeper"
   }
 }
 
 const validConfidence = ["Low", "High"];
 
-async function getPetFloors(){
+async function getPetFloors() {
   const response = await axios.get("https://dfk-floor-api.ew.r.appspot.com/petFloors")
     .catch(err => console.error(err));
 
-  if(response){
-    let retData = {};
-    for(const key in response.data){
-      let floorDetail = response.data[key];
-      retData[key] = validConfidence.includes(floorDetail.confidence) ? floorDetail : {"floorPrice": 0, "confidence": "Unknown"}
-    }
-    return retData;
-  }
-  else{
+  if (response && response.status === 200) {
+    return Object.entries(response.data)
+      .reduce((acc, [key, tainted]) => {
+        if (/pet:\d+:\d+/.test(key)) {
+          acc[key] = {
+            floorPrice: parseInt(tainted.floorPrice),
+            lastUpdated: parseInt(tainted.lastUpdated),
+            confidence: validConfidence.includes(tainted.confidence) ? tainted.confidence : "Unknown",
+          }
+        }
+
+        return acc
+      }, {});
+  } else {
     return false;
   }
 }
 
 
-export async function queryPets(address){
+export async function queryPets(address) {
 
   const petFloors = await getPetFloors();
 
   const petCount = {
-    sd:0,
-    cv:0,
-    sd2:0
+    sd: 0,
+    cv: 0,
+    sd2: 0
   };
 
   const petTotalValue = {
-    sd:0,
-    cv:0,
-    sd2:0
+    sd: 0,
+    cv: 0,
+    sd2: 0
   };
 
   const client = new GraphQLClient(graphqlapiUrl);
   let results = await client.request(petQuery, {ownerAddress: address});
   const pets = []
-  for(let petData of results.pets){
+  for (let petData of results.pets) {
     let pet = {...petData};
     pet.rarityString = Rarity[petData.rarity];
     pet.eggString = eggType[petData.eggType];
-    switch(petData.eggType){
+    switch (petData.eggType) {
       case 0:
-        pet.displayName = blueEggData.filter(pet => pet.appearanceId === petData.appearance )[0].displayName;
+        pet.displayName = blueEggData.filter(pet => pet.appearanceId === petData.appearance)[0].displayName;
         break;
       case 1:
-        pet.displayName = greyEggData.filter(pet => pet.appearanceId === petData.appearance )[0].displayName;
+        pet.displayName = greyEggData.filter(pet => pet.appearanceId === petData.appearance)[0].displayName;
         break;
       default:
         pet.displayName = 'Unknown';
@@ -265,22 +269,23 @@ export async function queryPets(address){
     pet.profStars = bonusStars[Math.floor(profBonusRef / 80)];
 
     pet.craftBonus = bonuses[petData.craftBonus - (petData.element * 10000)];
-    if(pet.craftBonus !== '-') pet.craftName = craftingTypes[petData.element]; //only show crafting type if a bonus is present
+    if (pet.craftBonus !== '-') pet.craftName = craftingTypes[petData.element]; //only show crafting type if a bonus is present
     pet.combatBonus = bonuses[petData.combatBonus]
     //set floors if available
-    if(petFloors){
+    if (petFloors) {
       let petkey = "pet:" + petData.eggType + ":" + petData.rarity;
       let floor = petFloors[petkey];
       pet.floorPrice = parseInt(floor.floorPrice);
-      pet.floorConfidence =  floor.confidence;
+      pet.floorConfidence = floor.confidence;
       petTotalValue[pet.chainName] += pet.floorPrice;
     }
     petCount[pet.chainName]++;
 
     pets.push(pet);
   }
-  return {pets: pets,
-          count: petCount,
-          totals: petTotalValue
-        };
+  return {
+    pets: pets,
+    count: petCount,
+    totals: petTotalValue
+  };
 }
